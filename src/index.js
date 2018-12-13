fe85ca5dc52f5b64348bbcba210487f22692d1ba
ChilenoSika <jaum97@yahoo.com.br>
2018-12-13 01:22:57 -0200

    Small stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const sum = (...params) => params.reduce((num, next) => num + next);
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-console.log(sum(1, 2, 3));
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+render(<App />, document.getElementById('app'));
